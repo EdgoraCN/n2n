cd394b897c0425a4fa66d22bfc3d8a6f2420547e
zhi-hu.wang <zhi-hu.wang@atos.net>
2020-08-05 23:37:28 +0800

    disable cache

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
-docker build -t registry.cn-beijing.aliyuncs.com/edgora-oss/n2n -f Dockerfile .
+docker build -t registry.cn-beijing.aliyuncs.com/edgora-oss/n2n --no-cache -f Dockerfile .
 
 docker tag  registry.cn-beijing.aliyuncs.com/edgora-oss/n2n  registry.cn-beijing.aliyuncs.com/edgora-oss/n2n:2.6.1
 
