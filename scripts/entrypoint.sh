cb2c4d0435252dffda76e04250cb4b2e8fc7413a
ahoo <ahoo@vip.qq.com>
2020-08-10 02:49:39 +0800

    fix docker issue for sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-POSITIONAL=()
+#POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
 key="$1"
@@ -26,12 +26,12 @@ case $key in
     shift # past argument
     ;;
     *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
+    #POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
     ;;
 esac
 done
-set -- "${POSITIONAL[@]}" # restore positional parameters
+#set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo edge_username=$edge_username
 echo edge_id=$edge_id
@@ -40,16 +40,16 @@ mkdir -p /tmp
 FILE=/tmp/edge-agent.conf
 if  [ -f "$FILE" ]; then
     echo "edge config file [$FILE] exists, starting..."
-    edge $FILE
+    /usr/local/bin/edge $FILE
 else 
     echo "try to active edge"
     curl -o /tmp/edge-agent.json https://console.edgora.com/api/client-tokens/active/$edge_username/$edge_id/$edge_code
     token=`cat /tmp/edge-agent.json | jj token`
     if [ -z "$token" ]; then
         echo "token is not found, exit"
-        cat  ~/.edge-agent/edge-agent.json
+        cat  /tmp/edge-agent.json
         exit 1;
     fi
-    curl -o ~/.edge-agent/edge-agent.conf http://10.9.9.120:9000/api/edge/conf/$token
-    edge $FILE
+    curl -o /tmp/edge-agent.conf https://console.edgora.com/api/edge/conf/$token
+    /usr/local/bin/edge $FILE
 fi
